edff01cab6ac3fc41f4093bec3d474f85e3119c5
lexeach <80155340+lexeach@users.noreply.github.com>
2023-07-19 18:14:56 +0530

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -204,9 +204,9 @@ const Dashboard = () => {
     event.preventDefault();
 
     let { amount } = topupVal;
-    amount = amount * 100;
+    //amount = amount * 100;
     // amount = amount * 1000000000000000000;
-    //amount = web3.utils.toWei(amount, "ether");
+    amount = web3.utils.toWei(amount, "ether");
 
     let ICU_ = new web3.eth.Contract(ICU.ABI, ICU.address);
     // let value_ = await ICU_.methods.REGESTRATION_FESS().call();
